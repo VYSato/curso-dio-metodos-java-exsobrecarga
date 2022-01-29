f88946c8ce4da63d1313e9b9e0d112c3a338f8ed
viniy <viniciussato2010@hotmail.com>
2022-01-29 01:27:49 -0300

    first commit

---
+++ src/dio/metodos/Main.java
@@ -0,0 +1,16 @@
+package dio.metodos;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(4);
+        Quadrilatero.area(6d,6d);
+        Quadrilatero.area(7,2,8);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
